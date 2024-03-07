32471d3e542accafdae530d62a603bf01c5f21c7
rezaakbar5 <162063302+rezaakbar5@users.noreply.github.com>
2024-03-07 14:03:47 +0700

    Add files via upload

--- socks.sh
+++ socks.sh
@@ -32,30 +32,30 @@ ipsaya=$(curl -sS ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
 data_ip="https://raw.githubusercontent.com/OknumA2Nyell/tunneling/main/ip"
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${merah}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      ${hijau}WhatsApp${NC} wa.me/6287715768324"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit 0
-  fi
-}
- if [[ $date_list > "2024-03-05" ]]; then
-   checking_sc
-  else
-  echo -ne
- fi
-echo ""
+# checking_sc() {
+  # useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  # if [[ $date_list < $useexp ]]; then
+    # echo -ne
+  # else
+    # echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    # echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    # echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    # echo -e ""
+    # echo -e "            ${merah}PERMISSION DENIED !${NC}"
+    # echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    # echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    # echo -e "             \033[0;33mContact Admin :${NC}"
+    # echo -e "      ${hijau}WhatsApp${NC} wa.me/6287715768324"
+    # echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    # exit 0
+  # fi
+# }
+ # if [[ $date_list > "2024-03-05" ]]; then
+   # checking_sc
+  # else
+  # echo -ne
+ # fi
+# echo ""
 read -p "$(echo -e "${cyan}Buat User Baru Untuk Socks5 : $putih")" usernya
 read -p "$(echo -e "${cyan}Buat Password Baru : $NC")" pass
 echo ""
